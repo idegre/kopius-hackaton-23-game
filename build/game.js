234b02fe06aee73207cf014d42bbbbd8130b6bfa
Ignacio <ignacio.degregori@gmail.com>
2023-08-10 16:34:57 -0300

    initial drawing game

--- build/game.js
+++ build/game.js
@@ -1,213 +1,95 @@
-var Brush;
-var BrushTip;
-var Tips = {};
-(function () {
-  "use strict";
-  function checkObject(obj) {
-    return typeof obj != "object" ? {} : obj;
-  }
-  function checkNumber(number, defaultValue) {
-    return typeof number != "number" ? defaultValue : number;
-  }
-  Brush = function (options) {
-    options = checkObject(options);
-    this.path = options.path || null;
-    this.ctx = options.ctx || null;
-    this.spacing = checkNumber(options.spacing, 10s);
-    this.tip = options.tip || new Tips.Rough1();
-    if (this.path != null) this.pathLength = this.path.getTotalLength();
-  };
-  Brush.prototype = {
-    path: null,
-    pathLength: 0,
-    v: 0,
-    lastV: 0,
-    ctx: null,
-    spacing: 0,
-    tip: null,
-    getPos: function (v) {
-      v = this._cleanV(v);
-      return this.path.getPointAtLength(v * this.pathLength);
-    },
-    draw: function () {
-      var v = this._getV();
-      this._render(v);
-      this.lastV = v;
-    },
-    drawFromLast: function () {
-      var v = this._getV();
-      var lastV = this.lastV;
-      var spacing = this.spacing;
-      if (spacing > 0) {
-        var dist = Math.abs(v - lastV);
-        var step = this._lenToV(spacing);
-        var steps = Math.ceil(dist / step);
-        step = dist / steps;
-        var curV = lastV;
-        var timeout = 1000;
-        while (
-          timeout > 0 &&
-          ((v > lastV && curV <= v) || (v < lastV && curV >= v))
-        ) {
-          timeout--;
-          this._render(curV, {
-            rotation: 0,
-            pressure: Math.sqrt(1 - Math.abs(v - 0.5) * 2),
-          });
-          curV += step;
-        }
-      }
-      this.lastV = v;
-    },
-    _vToLen: function (v) {
-      return v * this.pathLength;
-    },
-    _lenToV: function (len) {
-      return len / this.pathLength;
-    },
-    _getV: function () {
-      return this._cleanV(this.v);
-    },
-    _cleanV: function (v) {
-      v = checkNumber(v, this.v);
-      if (v > 1) v = 1;
-      if (v < 0) v = 0;
-      return v;
-    },
-    _render: function (v, options) {
-      v = this._cleanV(v);
-      options = checkObject(options);
-      var rotation = checkNumber(options.rotation, 0);
-      var pressure = checkNumber(options.pressure, 1);
-      var ctx = this.ctx;
-      var pos = this.getPos(v);
-      ctx.save();
-      ctx.translate(pos.x, pos.y);
-      this.tip.draw(ctx, {
-        pressure: pressure,
-      });
-      ctx.restore();
-    },
-  };
-  BrushTip = function (options) {
-    options = checkObject(options);
-    this.size = checkNumber(options.size, 1);
-  };
-  BrushTip.prototype = {
-    size: 1,
-    _currentCtx: null,
-    _currentOptions: {},
-    draw: function (ctx, options) {
-      options = checkObject(options);
-      this._currentCtx = ctx;
-      this._render(options);
-    },
-    _render: function (options) {},
-    _drawCircle: function (r, x, y, polar) {
-      x = checkNumber(x, 0);
-      y = checkNumber(y, 0);
-      if (polar) {
-        var a = x;
-        var d = y;
-        x = Math.cos(a) * d;
-        y = Math.sin(a) * d;
-      }
-      var ctx = this._currentCtx;
-      ctx.beginPath();
-      ctx.arc(x, y, r, 0, Math.PI * 2);
-      ctx.fill();
-      ctx.closePath();
-    },
-  };
-  Tips.Round = function (options) {
-    BrushTip.call(this, options);
-  };
-  Tips.Round.prototype = Object.create(BrushTip.prototype);
-  Tips.Round.prototype.constructor = Tips.Round;
-  Tips.Round.prototype._render = function (options) {
-    BrushTip.prototype._render.call(this, options);
-    var pressure = checkNumber(options.pressure, 1);
-    var ctx = this._currentCtx;
-    ctx.scale(1, 0.4);
-    this._drawCircle(6 * this.size);
-  };
-  // Rough brush
-  Tips.Rough1 = function (options) {
-    BrushTip.call(this, options);
-    var maxDist = 8;
-    this._points = new Array();
-    for (var i = 0; i < 15; i++) {
-      var d = Math.pow(Math.random(), 2);
-      var a = Math.random() * Math.PI * 2;
-      var r = 0.1 + Math.pow(1 - d, 2) * 4;
-      d *= maxDist;
-      this._points.push({
-        d: d,
-        a: a,
-        r: r,
-      });
+"use strict";
+console.log("begin");
+const drawHint = (ctx, h, w) => {
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'black';
+    const r = w * 0.4;
+    const center = w / 2;
+    //vertical
+    ctx.beginPath();
+    ctx.moveTo(w / 2, h * 0.1);
+    ctx.lineTo(w / 2, h * 0.9);
+    ctx.stroke();
+    ctx.closePath();
+    //diagonal1
+    ctx.beginPath();
+    ctx.moveTo(center + (r * Math.sin(45)), center + (r * Math.cos(45)));
+    ctx.lineTo(center - (r * Math.sin(45)), center - (r * Math.cos(45)));
+    ctx.stroke();
+    ctx.closePath();
+    //diagonal2
+    ctx.beginPath();
+    ctx.moveTo(center + (r * Math.sin(-45)), center + (r * Math.cos(-45)));
+    ctx.lineTo(center - (r * Math.sin(-45)), center - (r * Math.cos(-45)));
+    ctx.stroke();
+    ctx.closePath();
+};
+const pointDistance = (p1x, p1y, p2x, p2y) => {
+    const a = p1x - p2x;
+    const b = p1y - p2y;
+    return Math.sqrt(a * a + b * b);
+};
+//@ts-ignore
+const requestAnimationFrameOpt = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+window.requestAnimationFrame = requestAnimationFrame;
+let mouseDown = false;
+const render = () => {
+    console.log('loaded');
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    const svg = document.querySelector("svg");
+    let strokes = [];
+    const size = 300;
+    var scale = 3;
+    canvas.style.width = size + 'px';
+    canvas.style.height = size + 'px';
+    canvas.width = size * scale;
+    canvas.height = size * scale;
+    let points = [];
+    if (ctx) {
+        drawHint(ctx, size, size);
+        ctx.lineWidth = size / 12;
+        const gradient = ctx.createLinearGradient(0, size / 2, size, size / 2);
+        gradient.addColorStop(0, "rgb(247, 63, 93)");
+        gradient.addColorStop(0.2, "rgb(247, 63, 93)");
+        gradient.addColorStop(0.8, "rgb(153, 0, 127)");
+        gradient.addColorStop(1, "rgb(153, 0, 127)");
+        ctx.fillStyle = "rgb(247, 63, 93)";
+        ctx.strokeStyle = gradient;
+        ctx.lineCap = 'round';
+        canvas.addEventListener("mousedown", () => {
+            mouseDown = true;
+            ctx.beginPath();
+        });
+        canvas.addEventListener("mouseup", () => {
+            mouseDown = false;
+            ctx.stroke();
+            ctx.closePath();
+            strokes.push(points);
+            points = [];
+            console.log(strokes);
+        });
+        canvas.addEventListener("mousemove", (e) => {
+            if (mouseDown) {
+                const ex = e.offsetX * scale;
+                const ey = e.offsetY * scale;
+                if (points.length === 0) {
+                    ctx.moveTo(ex, ey);
+                    points.push([ex, ey]);
+                    ctx.stroke();
+                }
+                else {
+                    if (pointDistance(points[points.length - 1][0], points[points.length - 1][1], ex, ey) > 1) {
+                        ctx.lineTo(ex, ey);
+                        points.push([ex, ey]);
+                        ctx.stroke();
+                    }
+                }
+            }
+        });
     }
-    console.log(this);
-  };
-  Tips.Rough1.prototype = Object.create(BrushTip.prototype);
-  Tips.Rough1.prototype.constructor = Tips.Rough1;
-  Tips.Rough1.prototype._points = null;
-  Tips.Rough1.prototype._render = function (options) {
-    BrushTip.prototype._render.call(this, options);
-    var pressure = checkNumber(options.pressure, 1);
-    var size = this.size;
-    var ctx = this._currentCtx;
-    ctx.fillStyle = "rgb(0,0,0)";
-    var that = this;
-    this._points.forEach(function (point, i) {
-      that._drawCircle(point.r * pressure, point.a, point.d, true);
-    });
-  };
-})();
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    "use strict";
-    var ctx = document.querySelector("canvas").getContext("2d"),
-      svg = document.querySelector("svg"),
-      paths = svg.querySelectorAll("path"),
-      roughBrushes = [],
-      ovalBrushes = [];
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    for (var i = 0; i < paths.length; i++) {
-      var path = paths[i];
-      var roughBrush = new Brush({
-        path: path,
-        ctx: ctx,
-        spacing: 1,
-      });
-      roughBrushes.push(roughBrush);
-      var ovalBrush = new Brush({
-        path: path,
-        ctx: ctx,
-        spacing: 2,
-        tip: new Tips.Round(),
-      });
-      ovalBrushes.push(ovalBrush);
+    else {
+        console.error('canvas not suppoerted');
     }
-    function animateBrush(brush, i) {
-      var updateBrush = function () {
-        brush.drawFromLast();
-      };
-      ctx.fillStyle = "rgb(0, 0, 0)";
-      TweenMax.to(brush, 1, {
-        v: 1,
-        delay: 0.15 * i,
-        ease: Quint.easeOut,
-        onUpdate: updateBrush,
-        onComplete: updateBrush,
-      });
-    }
-    function animateRough() {
-      ctx.fillStyle = "rgb(0, 0, 0)";
-      roughBrushes.forEach(animateBrush);
-    }
-    animateRough();
-  },
-  false
-);
+};
+window.addEventListener('DOMContentLoaded', render);
